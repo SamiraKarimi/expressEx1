d9235d88ab13a54af113c293ead957f5ff237cab
Samira Karimi <samira_ka006@yahoo.com>
2019-04-22 02:55:22 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var app = express();
+
+app.get("/",function(req,res){
+    res.send("Hi there, welcome to my assignment!")
+});
+
+app.get("/speak/:animal",function(req,res){
+    var sounds = {
+        pig: "oink",
+        cat:"mew",
+        cow:"mow",
+        dog:"wof",
+        goldfish: "...",
+    }
+    var animal = req.params.animal.toLowerCase();
+    var sound = sounds[animal]
+    res.send("The " + animal + " says ' " + sound + " '" );
+});
+app.get("/repeat/:message/:times",function(req, res){
+    var message = req.params.message;
+    var times  = Number(req.params.times);
+    var result = "";
+    for(var i = 0; i<times; i++) {
+        result +=message + " ";
+    }
+    res.send(result);
+})
+
+app.get("/*/",function(req, res) {
+    res.send("sorry page can not found")
+    
+})
+
+app.listen(process.env.PORT,process.env.IP, function(){
+    console.log("Now serving your app!")
+})
